a27fb2471b4d62a95703e508ddd86829edd4ce65
Hypobeggar <89600337+Hypobeggar@users.noreply.github.com>
2024-09-01 18:29:56 -0500

    Update reserves

--- reserves_schema.sql
+++ reserves_schema.sql
@@ -1,4 +1,29 @@
 CREATE SCHEMA `reserves` ;
+
+CREATE TABLE `reserves`.`sailors` (
+  `Sname` VARCHAR(10) NULL,
+  `SID` INT NOT NULL,
+  `Rating` INT NULL,
+  `Age` INT NULL,
+  PRIMARY KEY (`SID`));
+
+INSERT INTO reserves.sailors (SID, Sname, Rating, Age) VALUES
+(23, "Marx", 8, 52),
+(25, "Martin", 9, 51),
+(27, "Adams", 8, 36),
+(33, "Carrey", 10, 22);
+
+CREATE TABLE `reserves`.`boats` (
+  `Bname` VARCHAR(15) NULL,
+  `BID` INT NOT NULL,
+  `Fee` DECIMAL(10,2) NULL,
+  `Location` VARCHAR(45) NULL,
+  PRIMARY KEY (`BID`));
+
+INSERT INTO reserves.boats (BID, Bname, Fee, Location)  
+VALUES (109, "Wayfarer", 120, "Hout Bay"), (108, "SeaPride", 500, "Fish Hoek"), (101, "Yuppie", 400, "Hout Bay"), (104, "Joy", 200, "Hout Bay");
+
+
 CREATE TABLE `reserves`.`reserves` (
   `SID` INT NOT NULL,
   `BID` INT NOT NULL,
