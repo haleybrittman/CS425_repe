d8744f526faffb07701818dd3c96fb97b512a7f3
Hypobeggar <89600337+Hypobeggar@users.noreply.github.com>
2024-10-25 15:24:03 -0500

    Create menu

--- project_app/main.py
+++ project_app/main.py
@@ -12,7 +12,8 @@ try:
     cursor = database.cursor()
 
 
-
+    def showTables():
+        query= "SHOW TABLES"
 
     def read_data():
         select_query = "SELECT Bus_Type FROM bus"
@@ -25,6 +26,25 @@ try:
         for bus_type in bus_types:
             print(bus_type)
 
-    read_data()
+    def main():
+        while True:
+            print('Menu\n1. Read a table\'s data')
+            print('2. Insert data into a table\n3. Update data in a table\n4. Delete data')
+            print('5. Show all tables\n6. Exit')
+
+            menuChoice = input('What would you like to do?')
+            if menuChoice == 1:
+                read_data()
+    if __name__ == "__main__":
+        main()
 except mysql.connector.Error as e:
     print("Error:", e)
+
+
+
+finally:
+    # Close DB connection
+    if database.is_connected():
+        cursor.close()
+        database.close()
+        print("Closed connection to Transit System Database.")
