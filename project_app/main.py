5762128768485776a267135a1ddd4ba44dc607ef
Hypobeggar <89600337+Hypobeggar@users.noreply.github.com>
2024-10-26 19:57:40 -0500

    Add insert/ Create functionality

--- project_app/main.py
+++ project_app/main.py
@@ -14,7 +14,7 @@ try:
     cursor = database.cursor()
 
 
-    def showTables():
+    def showTables(): # Displays tables excluding views
         cursor.execute("show full tables where Table_Type != 'VIEW'")
         tables = cursor.fetchall()
         print("\nAvailable tables:")
@@ -22,7 +22,7 @@ try:
             print(f"{idx + 1}. {table[0]}")
         return
 
-    def selectTable():
+    def selectTable(): # Reused for options so user can select which table they're using for the option
         table_dict = {
         '1': 'bus',
         '2': 'bus_bus_route',
@@ -39,9 +39,10 @@ try:
         '13': 'train_stop',
         '14': 'train_stops_time'}
     
-        while True:
+        while True: # Error checking for invalid number
             number = input('Which table would you like to use? Enter a number: ')
             if number in table_dict:
+                print('')
                 return table_dict[number]
             else:
                 print('Please select a valid number')
@@ -49,7 +50,7 @@ try:
         
 
 
-    def read_data(table):
+    def read_data(table): # Read function. Reads data
         select_query = f"SELECT * FROM {table}"
         cursor.execute(select_query)
         
@@ -62,9 +63,32 @@ try:
         for row in rows:
             print(row)
 
-    def insert_data(table):
-        return
-    def update_data(table):
+    def insert_data(table): # Create function. Insertion of data
+        table_dict={ # Error checking is handled by selectTable()
+        'bus':'(bus_id,bus_type,Wheelchair_accessibility,Capacity)',
+        'bus_bus_route':'(Bus_ID, Route_no)',
+        'bus_route':'(route_no, name, direction)',
+        'bus_route_bus_stops':'(Stop_name, Route_no)',
+        'bus_stops':'(Stop_Name, Route, Stop_Order)',
+        'bus_stops_stop_order':'(Stop_Name, Location, Route, Stop_Order)',
+        'bus_stops_time':'(Stop_Name, Route, Time, ETA)',
+        'line':'(ColorType, Name, Direction, Start)',
+        'line_train_stops':'(ColorType, Name, Direction, Station_Name)',
+        'passenger':'(Name, Passenger_ID, Disabled, Phone_Number, train_id, bus_id)',
+        'train':'(Train_ID, Capacity)',
+        'train_line':'(Train_ID, ColorType, Name, Direction)',
+        'train_stop':'(Station_Name, ColorType, Name, Direction)',
+        'train_stops_time':'(TID, Station_Name, ColorType, Line_Name, Direction, Time)'}
+        values=input(f'Input values in the following format: {table_dict[table]}: ')
+        insert_query = f"INSERT INTO {table} VALUES {values}"
+        
+        try:
+            cursor.execute(insert_query)
+        except Exception as e:
+            print('Error:', e)
+
+
+    def update_data(table): # Update function. Updates data in a row
         column= input('Update which column? ')
         column_content= input('Set the column to what? ')
         row_change= input('Where... ')
@@ -77,7 +101,7 @@ try:
             print('Error:', e)
 
 
-    def delete_data(table):
+    def delete_data(table): # Delete function. Delete a row
         row_delete= input('Delete from '+table+' where... ')
         row_content=input('equals... ')
         delete_query = f"DELETE FROM {table} WHERE {row_delete} = {row_content}"
@@ -87,7 +111,7 @@ try:
         except Exception as e:
             print('Error:', e)
 
-    def main():
+    def main(): # Main function. Displays menu until user exits
         while True:
             print('\nMenu\n1. Read a table\'s data')
             print('2. Insert data into a table\n3. Update data in a table\n4. Delete data')
